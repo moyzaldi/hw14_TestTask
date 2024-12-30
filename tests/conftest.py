a8ab52df434cc5dc2f1006ba982c0f79a1523af8
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-30 19:47:19 +0300

    Добавлен запуск  в селеноид

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,67 @@
 import allure
 import pytest
+import os
+
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+from utils import attach
 
 
-@pytest.fixture()
-def browser_settings():
-    with allure.step('Параметры браузера'):
-        driver_options = webdriver.ChromeOptions()
-        driver_options.page_load_strategy = 'eager'
-        browser.config.driver_options = driver_options
+DEFAULT_BROWSER_VERSION = '126.0'
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='126.0'
+    )
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def browser_settings(request):
+    with allure.step("Параметры браузера"):
+        browser_version = request.config.getoption('--browser_version')
+        browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        }
+
+        options.capabilities.update(selenoid_capabilities)
+        login = os.getenv('LOGIN')
+        password = os.getenv('PASSWORD')
+        selenoid_host = os.getenv('SELENOID_HOST')
+
+
+        driver = webdriver.Remote(
+            command_executor=f"https://{login}:{password}@{selenoid_host}/wd/hub",
+            options=options)
+
+        browser.config.driver = driver
+
+        browser.config.base_url = 'https://russoutdoor.ru/'
+        options.page_load_strategy = 'eager'
         browser.config.window_height = 1080
         browser.config.window_width = 1920
-        browser.config.base_url = 'https://russoutdoor.ru/'
 
-    yield
-    with allure.step('Закрываем браузер'):
+        yield
+
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
+        attach.add_video(browser)
+
         browser.quit()
 
-    browser.config.base_url = 'https://russoutdoor.ru/'
+
+
